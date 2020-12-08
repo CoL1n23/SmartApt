afb6e51f466240d3584cc9b41ff94264d0dd954c
Colin Wu <colinwu1418@gmail.com>
2020-12-08 15:16:07 -0500

    version 3.0 - final version

--- tables/droptables.sql
+++ tables/droptables.sql
@@ -6,12 +6,3 @@ DROP table Cars;
 DROP table School;
 DROP table Company;
 DROP table LivingHabit;
-
-DROP INDEX resident_index;
-DROP INDEX student_index;
-DROP INDEX employee_index;
-DROP INDEX room_index;
-DROP INDEX cars_index;
-DROP INDEX school_index;
-DROP INDEX company_index;
-DROP INDEX habit_index;
